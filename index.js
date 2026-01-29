d254e3203b304d59f73175a3f40ea794922a597e
owainbrowne <owain_browne@hotmail.com>
2026-01-29 16:26:33 +0000

    removed cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 
 app.use(express.static('dist'))
 
-app.use(cors())
+// app.use(cors())
 
 let notes = [
   {
